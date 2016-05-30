9ec3987dd5c952daa9a77c3fd696e9ff9e3d0696
Nobody Random <iamnobodyrandom@yahoo.com>
2016-05-29 21:13:14 -0400

    ...

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -120,6 +120,68 @@ function main() {
 main();
 
 $(document).ready(function() {
+    // Auto resize input
+    function resizeInput() {
+        $(this).attr('size', $(this).val().length);
+    }
+
+    $('input[type="text"], input[type="email"]')
+    // event handler
+        .keyup(resizeInput)
+        // resize on page load
+        .each(resizeInput);
+// Adapted from georgepapadakis.me/demo/expanding-textarea.html
+    (function(){
+        var textareas = document.querySelectorAll('.expanding'),
+
+            resize = function(t) {
+                t.style.height = 'auto';
+                t.style.overflow = 'hidden'; // Ensure scrollbar doesn't interfere with the true height of the text.
+                t.style.height = (t.scrollHeight + t.offset ) + 'px';
+                t.style.overflow = '';
+            },
+
+            attachResize = function(t) {
+                if ( t ) {
+                    console.log('t.className',t.className);
+                    t.offset = !window.opera ? (t.offsetHeight - t.clientHeight) : (t.offsetHeight + parseInt(window.getComputedStyle(t, null).getPropertyValue('border-top-width')));
+
+                    resize(t);
+
+                    if ( t.addEventListener ) {
+                        t.addEventListener('input', function() { resize(t); });
+                        t.addEventListener('mouseup', function() { resize(t); }); // set height after user resize
+                    }
+
+                    t['attachEvent'] && t.attachEvent('onkeyup', function() { resize(t); });
+                }
+            };
+
+        // IE7 support
+        if ( !document.querySelectorAll ) {
+            function getElementsByClass(searchClass,node,tag) {
+                var classElements = new Array();
+                node = node || document;
+                tag = tag || '*';
+                var els = node.getElementsByTagName(tag);
+                var elsLen = els.length;
+                var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+                for (i = 0, j = 0; i < elsLen; i++) {
+                    if ( pattern.test(els[i].className) ) {
+                        classElements[j] = els[i];
+                        j++;
+                    }
+                }
+                return classElements;
+            }
+            textareas = getElementsByClass('expanding');
+        }
+
+        for (var i = 0; i < textareas.length; i++ ) {
+            attachResize(textareas[i]);
+        }
+    })();
+
     $('form').submit(function(e) {
         if (e.preventDefault) e.preventDefault();
         else e.returnValue = false;
@@ -168,10 +230,16 @@ $(document).ready(function() {
                         // Throw error message
                         document.getElementById("submit").disabled = false;
                     }
+                    document.getElementById("email").value = "";
+                    document.getElementById("your-message").value = "";
+                    document.getElementById("your-name").value = "";
                 },
                 error: function (error) {
                     console.log(error);
                     // Throw error message
+                    document.getElementById("email").value = "";
+                    document.getElementById("your-message").value = "";
+                    document.getElementById("your-name").value = "";
                 }
             });
         }
