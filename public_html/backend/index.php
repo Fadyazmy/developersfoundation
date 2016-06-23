c0989a76b16a1ea982b7fc34d95abe5b3bbf44a7
Nobody Random <iamnobodyrandom@yahoo.com>
2016-06-23 17:26:47 -0400

    init backend

--- public_html/backend/index.php
+++ public_html/backend/index.php
@@ -394,7 +394,7 @@ echo('<tr/><tr><td><a href=\'' . $editLinkValue . '\'>' . 'Edit User' . '</a></t
         <!-- page content -->
         <div class="right_col" role="main">
             <!-- top tiles -->
-            <div class="row tile_count">
+            <!--<div class="row tile_count">
                 <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
                     <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
                     <div class="count">2500</div>
@@ -430,10 +430,10 @@ echo('<tr/><tr><td><a href=\'' . $editLinkValue . '\'>' . 'Edit User' . '</a></t
                     <span class="count_bottom"><i class="green"><i
                                 class="fa fa-sort-asc"></i>34% </i> From last Week</span>
                 </div>
-            </div>
+            </div>-->
             <!-- /top tiles -->
 
-            <div class="row">
+            <!--<div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="dashboard_graph">
 
@@ -943,11 +943,11 @@ echo('<tr/><tr><td><a href=\'' . $editLinkValue . '\'>' . 'Edit User' . '</a></t
                         </div>
 
                     </div>
-                    <div class="row">
+                    <div class="row">-->
 
 
                         <!-- Start to do list -->
-                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        <!--<div class="col-md-6 col-sm-6 col-xs-12">
                             <div class="x_panel">
                                 <div class="x_title">
                                     <h2>To Do List
@@ -1024,11 +1024,11 @@ echo('<tr/><tr><td><a href=\'' . $editLinkValue . '\'>' . 'Edit User' . '</a></t
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div>-->
                         <!-- End to do list -->
 
                         <!-- start of weather widget -->
-                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        <!--<div class="col-md-6 col-sm-6 col-xs-12">
                             <div class="x_panel">
                                 <div class="x_title">
                                     <h2>Daily active users
@@ -1135,11 +1135,11 @@ echo('<tr/><tr><td><a href=\'' . $editLinkValue . '\'>' . 'Edit User' . '</a></t
                                 </div>
                             </div>
 
-                        </div>
+                        </div>-->
                         <!-- end of weather widget -->
-                    </div>
+                    <!--</div>
                 </div>
-            </div>
+            </div>-->
         </div>
         <!-- /page content -->
 
