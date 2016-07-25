cb45623326e00472f56df66b4af009a6d4525dc9
Nobody Random <iamnobodyrandom@yahoo.com>
2016-07-25 16:53:25 +0800

    test parse file for database file uploads

---
+++ public_html/backend/parse-db/testParseFile.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harrisonchow
+ * Date: 7/25/16
+ * Time: 4:47 PM
+ */
+
+// THIS CODE IS PURELY TO TEST IF THE DB CAN STORE FILES AND REUSE. SINCE HEROKU DOES NOT ALLOW PERSISTENT STORAGE
+
+require_once "../../../vendor/autoload.php";
+use Parse\ParseFile;
+
+session_start();
+
+// Heroku stores to /tmp/ but need to find a way to fetch it to store into db
+//$localFilePath = "/tmp/myFile.txt";
+$localFilePath = "test.png";
+$file = ParseFile::createFromFile($localFilePath, "test.png");
+$file->save();
+// The file has been saved to Parse and now has a URL.
+$url = $file->getURL();
+echo $url;
