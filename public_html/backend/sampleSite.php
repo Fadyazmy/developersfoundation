bd402246c66123d81498789da18d7c926ae98c93
Nobody Random <iamnobodyrandom@yahoo.com>
2016-08-16 14:29:08 +0800

    sample site

---
+++ public_html/backend/sampleSite.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harrisonchow
+ * Date: 8/16/16
+ * Time: 2:24 PM
+ */
+
+require_once '../../vendor/autoload.php';
+use Parse\ParseClient;
+use Parse\ParseQuery;
+use Parse\ParseException;
+
+$ParseAppID = "developers-foundation-db";
+$ParseMasterKey = "Abcd1234";
+$ParseServer = "https://developers-foundation-db.herokuapp.com/parse";
+ParseClient::initialize($ParseAppID, '', $ParseMasterKey);
+ParseClient::setServerURL($ParseServer);
+
+$query = new ParseQuery("Website");
+$theWebsite = $query->get("5Itqbrk0en");
+print_r($theWebsite);
