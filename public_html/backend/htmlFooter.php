2a68c22a76203be326cefba334522668bd08259e
Nobody Random <iamnobodyrandom@yahoo.com>
2016-09-03 17:44:41 -0400

    init users php

--- public_html/backend/htmlFooter.php
+++ public_html/backend/htmlFooter.php
@@ -7,11 +7,15 @@
  */
 
 $websiteSubmit = false;
+$analytics = false;
 
 if (isset($includeFile) && $includeFile != "") {
     switch ($includeFile) {
         case "website-submit":
             $websiteSubmit = true;
+            break;
+        case "analytics-main":
+            $analytics = true;
             break;
     }
 }
@@ -275,6 +279,447 @@ if($websiteSubmit) {
     <script src="vendors/pnotify/dist/pnotify.buttons.js"></script>
     <script src="vendors/pnotify/dist/pnotify.nonblock.js"></script>';
     echo "<script src='assets/js/website-submit.js'></script>";
+} elseif ($analytics) {
+    echo '
+    <!-- jQuery Smart Wizard -->
+<script>
+    $(document).ready(function () {
+        $(\'#wizard\').smartWizard();
+
+        $(\'#wizard_verticle\').smartWizard({
+            transitionEffect: \'slide\'
+        });
+
+        $(\'.buttonPrevious\').addClass(\'btn btn-primary\');
+        $(\'.buttonNext\').addClass(\'btn btn-success\');
+        $(\'.buttonFinish\').addClass(\'btn btn-default\');
+    });
+</script>
+<!-- /jQuery Smart Wizard -->
+
+<!-- Flot -->
+<script>
+    $(document).ready(function () {
+        var data1 = [
+            [gd(2012, 1, 1), 17],
+            [gd(2012, 1, 2), 74],
+            [gd(2012, 1, 3), 6],
+            [gd(2012, 1, 4), 39],
+            [gd(2012, 1, 5), 20],
+            [gd(2012, 1, 6), 85],
+            [gd(2012, 1, 7), 7]
+        ];
+
+        var data2 = [
+            [gd(2012, 1, 1), 82],
+            [gd(2012, 1, 2), 23],
+            [gd(2012, 1, 3), 66],
+            [gd(2012, 1, 4), 9],
+            [gd(2012, 1, 5), 119],
+            [gd(2012, 1, 6), 6],
+            [gd(2012, 1, 7), 9]
+        ];
+        $("#canvas_dahs").length && $.plot($("#canvas_dahs"), [
+            data1, data2
+        ], {
+            series: {
+                lines: {
+                    show: false,
+                    fill: true
+                },
+                splines: {
+                    show: true,
+                    tension: 0.4,
+                    lineWidth: 1,
+                    fill: 0.4
+                },
+                points: {
+                    radius: 0,
+                    show: true
+                },
+                shadowSize: 2
+            },
+            grid: {
+                verticalLines: true,
+                hoverable: true,
+                clickable: true,
+                tickColor: "#d5d5d5",
+                borderWidth: 1,
+                color: \'#fff\'
+            },
+            colors: ["rgba(38, 185, 154, 0.38)", "rgba(3, 88, 106, 0.38)"],
+            xaxis: {
+                tickColor: "rgba(51, 51, 51, 0.06)",
+                mode: "time",
+                tickSize: [1, "day"],
+                //tickLength: 10,
+                axisLabel: "Date",
+                axisLabelUseCanvas: true,
+                axisLabelFontSizePixels: 12,
+                axisLabelFontFamily: \'Verdana, Arial\',
+                axisLabelPadding: 10
+            },
+            yaxis: {
+                ticks: 8,
+                tickColor: "rgba(51, 51, 51, 0.06)",
+            },
+            tooltip: false
+        });
+
+        function gd(year, month, day) {
+            return new Date(year, month - 1, day).getTime();
+        }
+    });
+</script>
+<!-- /Flot -->
+
+<!-- Flot for graph-->
+<script>
+    $(document).ready(function () {
+        //define chart clolors ( you maybe add more colors if you want or flot will add it automatic )
+        var chartColours = [\'#96CA59\', \'#3F97EB\', \'#72c380\', \'#6f7a8a\', \'#f7cb38\', \'#5a8022\', \'#2c7282\'];
+
+        //generate random number for charts
+        randNum = function () {
+            return (Math.floor(Math.random() * (1 + 40 - 20))) + 20;
+        };
+
+        var d1 = [];
+        //var d2 = [];
+
+        //here we generate data for chart
+        for (var i = 0; i < 30; i++) {
+            d1.push([new Date(Date.today().add(i).days()).getTime(), randNum() + i + i + 10]);
+            //    d2.push([new Date(Date.today().add(i).days()).getTime(), randNum()]);
+        }
+
+        var chartMinDate = d1[0][0]; //first day
+        var chartMaxDate = d1[20][0]; //last day
+
+        var tickSize = [1, "day"];
+        var tformat = "%d/%m/%y";
+
+        //graph options
+        var options = {
+            grid: {
+                show: true,
+                aboveData: true,
+                color: "#3f3f3f",
+                labelMargin: 10,
+                axisMargin: 0,
+                borderWidth: 0,
+                borderColor: null,
+                minBorderMargin: 5,
+                clickable: true,
+                hoverable: true,
+                autoHighlight: true,
+                mouseActiveRadius: 100
+            },
+            series: {
+                lines: {
+                    show: true,
+                    fill: true,
+                    lineWidth: 2,
+                    steps: false
+                },
+                points: {
+                    show: true,
+                    radius: 4.5,
+                    symbol: "circle",
+                    lineWidth: 3.0
+                }
+            },
+            legend: {
+                position: "ne",
+                margin: [0, -25],
+                noColumns: 0,
+                labelBoxBorderColor: null,
+                labelFormatter: function (label, series) {
+                    // just add some space to labes
+                    return label + \'&nbsp;&nbsp;\';
+                },
+                width: 40,
+                height: 1
+            },
+            colors: chartColours,
+            shadowSize: 0,
+            tooltip: true, //activate tooltip
+            tooltipOpts: {
+                content: "%s: %y.0",
+                xDateFormat: "%d/%m",
+                shifts: {
+                    x: -30,
+                    y: -50
+                },
+                defaultTheme: false
+            },
+            yaxis: {
+                min: 0
+            },
+            xaxis: {
+                mode: "time",
+                minTickSize: tickSize,
+                timeformat: tformat,
+                min: chartMinDate,
+                max: chartMaxDate
+            }
+        };
+        var plot = $.plot($("#placeholder33x"), [{
+            label: "Email Sent",
+            data: d1,
+            lines: {
+                fillColor: "rgba(150, 202, 89, 0.12)"
+            }, //#96CA59 rgba(150, 202, 89, 0.42)
+            points: {
+                fillColor: "#fff"
+            }
+        }], options);
+    });
+</script>
+<!-- /Flot -->
+
+<!-- Doughnut Chart -->
+<script>
+    $(document).ready(function () {
+        var options = {
+            legend: false,
+            responsive: false
+        };
+
+        new Chart(document.getElementById("canvas1"), {
+            type: \'doughnut\',
+            tooltipFillColor: "rgba(51, 51, 51, 0.55)",
+            data: {
+                labels: [
+                    "(Not Set)",
+                    "Macintosh",
+                    "Windows"
+                ],
+                datasets: [{
+                    data: [6, 52, 7],
+                    backgroundColor: [
+                        // "#BDC3C7",
+                        "#9B59B6",
+                        //"#E74C3C",
+                        "#26B99A",
+                        "#3498DB"
+                    ],
+                    hoverBackgroundColor: [
+                        //"#CFD4D8",
+                        "#B370CF",
+                        //"#E95E4F",
+                        "#36CAAB",
+                        "#49A9EA"
+                    ]
+                }]
+            },
+            options: options
+        });
+        new Chart(document.getElementById("canvas2"), {
+            type: \'doughnut\',
+            tooltipFillColor: "rgba(51, 51, 51, 0.55)",
+            data: {
+                labels: [
+                    "Safari",
+                    "Edge",
+                    "Mozilla",
+                    "Chrome"
+                ],
+                datasets: [{
+                    data: [5, 20, 10, 65],
+                    backgroundColor: [
+                        "#BDC3C7",
+                        "#9B59B6",
+                        "#26B99A",
+                        "#3498DB"
+                    ],
+                    hoverBackgroundColor: [
+                        "#CFD4D8",
+                        "#B370CF",
+                        "#36CAAB",
+                        "#49A9EA"
+                    ]
+                }]
+            },
+            options: options
+        });
+        new Chart(document.getElementById("canvas3"), {
+            type: \'doughnut\',
+            tooltipFillColor: "rgba(51, 51, 51, 0.55)",
+            data: {
+                labels: [
+                    "Gdynia",
+                    "Rivadavia",
+                    "Honolulu",
+                    "Toronto",
+                    "(Not Set)"
+                ],
+                datasets: [{
+                    data: [1, 1, 1, 7, 58],
+                    backgroundColor: [
+                        "#BDC3C7",
+                        "#9B59B6",
+                        "#E74C3C",
+                        "#26B99A",
+                        "#3498DB"
+                    ],
+                    hoverBackgroundColor: [
+                        "#CFD4D8",
+                        "#B370CF",
+                        "#E95E4F",
+                        "#36CAAB",
+                        "#49A9EA"
+                    ]
+                }]
+            },
+            options: options
+        });
+    });
+</script>
+<!-- /Doughnut Chart -->
+
+<!-- JQVMap -->
+<script>
+    $(document).ready(function () {
+        $(\'#world-map-gdp\').vectorMap({
+            map: \'world_en\',
+            backgroundColor: null,
+            color: \'#ffffff\',
+            hoverOpacity: 0.7,
+            selectedColor: \'#666666\',
+            enableZoom: true,
+            showTooltip: true,
+            values: sample_data,
+            scaleColors: [\'#E6F2F0\', \'#149B7E\'],
+            normalizeFunction: \'polynomial\'
+        });
+    });
+</script>
+<!-- /JQVMap -->
+
+<!-- bootstrap-daterangepicker -->
+<script>
+    $(document).ready(function () {
+
+        var cb = function (start, end, label) {
+            console.log(start.toISOString(), end.toISOString(), label);
+            $(\'#reportrange span\').html(start.format(\'MMMM D, YYYY\') + \' - \' + end.format(\'MMMM D, YYYY\'));
+        };
+
+        var optionSet1 = {
+            startDate: moment().subtract(29, \'days\'),
+            endDate: moment(),
+            minDate: \'01/01/2012\',
+            maxDate: \'12/31/2015\',
+            dateLimit: {
+                days: 60
+            },
+            showDropdowns: true,
+            showWeekNumbers: true,
+            timePicker: false,
+            timePickerIncrement: 1,
+            timePicker12Hour: true,
+            ranges: {
+                \'Today\': [moment(), moment()],
+                \'Yesterday\': [moment().subtract(1, \'days\'), moment().subtract(1, \'days\')],
+                \'Last 7 Days\': [moment().subtract(6, \'days\'), moment()],
+                \'Last 30 Days\': [moment().subtract(29, \'days\'), moment()],
+                \'This Month\': [moment().startOf(\'month\'), moment().endOf(\'month\')],
+                \'Last Month\': [moment().subtract(1, \'month\').startOf(\'month\'), moment().subtract(1, \'month\').endOf(\'month\')]
+            },
+            opens: \'left\',
+            buttonClasses: [\'btn btn-default\'],
+            applyClass: \'btn-small btn-primary\',
+            cancelClass: \'btn-small\',
+            format: \'MM/DD/YYYY\',
+            separator: \' to \',
+            locale: {
+                applyLabel: \'Submit\',
+                cancelLabel: \'Clear\',
+                fromLabel: \'From\',
+                toLabel: \'To\',
+                customRangeLabel: \'Custom\',
+                daysOfWeek: [\'Su\', \'Mo\', \'Tu\', \'We\', \'Th\', \'Fr\', \'Sa\'],
+                monthNames: [\'January\', \'February\', \'March\', \'April\', \'May\', \'June\', \'July\', \'August\', \'September\', \'October\', \'November\', \'December\'],
+                firstDay: 1
+            }
+        };
+        $(\'#reportrange span\').html(moment().subtract(29, \'days\').format(\'MMMM D, YYYY\') + \' - \' + moment().format(\'MMMM D, YYYY\'));
+        $(\'#reportrange\').daterangepicker(optionSet1, cb);
+        $(\'#reportrange\').on(\'show.daterangepicker\', function () {
+            console.log("show event fired");
+        });
+        $(\'#reportrange\').on(\'hide.daterangepicker\', function () {
+            console.log("hide event fired");
+        });
+        $(\'#reportrange\').on(\'apply.daterangepicker\', function (ev, picker) {
+            console.log("apply event fired, start/end dates are " + picker.startDate.format(\'MMMM D, YYYY\') + " to " + picker.endDate.format(\'MMMM D, YYYY\'));
+        });
+        $(\'#reportrange\').on(\'cancel.daterangepicker\', function (ev, picker) {
+            console.log("cancel event fired");
+        });
+        $(\'#options1\').click(function () {
+            $(\'#reportrange\').data(\'daterangepicker\').setOptions(optionSet1, cb);
+        });
+        $(\'#options2\').click(function () {
+            $(\'#reportrange\').data(\'daterangepicker\').setOptions(optionSet2, cb);
+        });
+        $(\'#destroy\').click(function () {
+            $(\'#reportrange\').data(\'daterangepicker\').remove();
+        });
+    });
+</script>
+<!-- /bootstrap-daterangepicker -->
+
+<!-- jQuery Sparklines -->
+<script>
+    $(document).ready(function () {
+        $(".sparkline_one").sparkline([2, 4, 3, 4, 5, 4, 5, 4, 3, 4, 5, 6, 4, 5, 6, 3, 5, 4, 5, 4, 5, 4, 3, 4, 5, 6, 7, 5, 4, 3, 5, 6], {
+            type: \'bar\',
+            height: \'125\',
+            barWidth: 13,
+            colorMap: {
+                \'7\': \'#a1a1a1\'
+            },
+            barSpacing: 2,
+            barColor: \'#26B99A\'
+        });
+
+        $(".sparkline11").sparkline([0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1, 0, 4, 0, 0, 30, 0, 23, 0, 3, 0], {
+            type: \'line\',
+            height: \'40\',
+            width: \'200\',
+            lineColor: \'#26B99A\',
+            fillColor: \'#ffffff\',
+            lineWidth: 3,
+            spotColor: \'#34495E\',
+            minSpotColor: \'#34495E\'
+        });
+
+        $(".sparkline22").sparkline([0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1, 0, 4, 0, 0, 30, 0, 23, 0, 3, 0], {
+            type: \'line\',
+            height: \'40\',
+            width: \'200\',
+            lineColor: \'#26B99A\',
+            fillColor: \'#ffffff\',
+            lineWidth: 3,
+            spotColor: \'#34495E\',
+            minSpotColor: \'#34495E\'
+        });
+        $(".sparkline33").sparkline([0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1, 0, 4, 0, 0, 30, 0, 23, 0, 3, 0], {
+            type: \'line\',
+            height: \'40\',
+            width: \'200\',
+            lineColor: \'#26B99A\',
+            fillColor: \'#ffffff\',
+            lineWidth: 3,
+            spotColor: \'#34495E\',
+            minSpotColor: \'#34495E\'
+        });
+    });
+</script>
+<!-- /jQuery Sparklines -->
+    ';
 }
 ?>
 
