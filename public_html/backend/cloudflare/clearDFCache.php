6b2792e4f359658bb658979a0003ef1b34c0adef
Nobody Random <iamnobodyrandom@yahoo.com>
2016-09-27 01:06:34 -0400

    clear cf cache

---
+++ public_html/backend/cloudflare/clearDFCache.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harrisonchow
+ * Date: 9/27/16
+ * Time: 1:02 AM
+ */
+
+$url = "https://api.cloudflare.com/client/v4/zones/0be7b1daf9a7e94109bb013e1ef0e455/purge_cache";
+$ch = curl_init();
+$json = json_encode(array("purge_everything" => true));
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "X-Auth-Email: iamnobodyrandom@yahoo.com",
+    "X-Auth-Key: 65b81ca3d2ea8e07282b7d173c6ea6041d194",
+    "Content-Type: application/json"
+));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+$result = json_decode($result);
+curl_close($ch);
+
+var_dump($result);
