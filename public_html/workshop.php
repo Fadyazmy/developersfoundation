1c9e236ca047c8ced2702420d232273e2567f29c
Nobody Random <iamnobodyrandom@yahoo.com>
2016-11-26 13:16:53 -0500

    typo

---
+++ public_html/workshop.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harrisonchow
+ * Date: 11/26/16
+ * Time: 1:15 PM
+ */
+
+
+header('Location: https://github.com/Developers-Foundation/2016-web-intro/archive/master.zip');
+exit;
