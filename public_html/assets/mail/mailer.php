c53c3c97983a37f594d514fab7a7fd4c6e6a09cf
Nobody Random <iamnobodyrandom@yahoo.com>
2017-01-25 18:46:52 -0500

    env var

--- public_html/assets/mail/mailer.php
+++ public_html/assets/mail/mailer.php
@@ -14,7 +14,7 @@ if($_SERVER['REQUEST_METHOD'] === 'GET'){
 require '../../../vendor/autoload.php';
 $getPost = (array)json_decode(file_get_contents('php://input'));
 
-$sendgrid = new SendGrid('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');
+$sendgrid = new SendGrid($_ENV["SENDGRID_API_KEY"]);
 $email = new SendGrid\Email();
 
 $email
