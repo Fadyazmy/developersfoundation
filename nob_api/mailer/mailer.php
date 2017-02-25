c47038c703f44a2b269ddd434d5fc6fb20988395
Nobody Random <iamnobodyrandom@yahoo.com>
2017-02-25 16:18:16 -0500

    add
    Signed-off-by: Nobody Random <iamnobodyrandom@yahoo.com>

--- nob_api/mailer/mailer.php
+++ nob_api/mailer/mailer.php
@@ -14,26 +14,14 @@ if($_SERVER['REQUEST_METHOD'] === 'GET'){
 require '../../../vendor/autoload.php'; // MAKE SURE THIS POINTS TO YOUR COMPOSER VENDOR FOLDER
 $getPost = (array)json_decode(file_get_contents('php://input'));
 
-$sendgrid = new SendGrid($_ENV["SENDGRID_API_KEY"]); // MAKE SURE THE API KEY IS INSIDE THE ENV VARIABLE
-$email = new SendGrid\Email();
+$from = new SendGrid\Email($getPost['fromName'], $getPost['sendFrom']);
+$subject = $getPost['subject'];
+$to = new SendGrid\Email($getPost['toName'], $getPost['sendTo']);
+$content = new SendGrid\Content("text/plain", $getPost['msgHTML']);
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$email
-    ->addTo($getPost['sendTo'])
-    ->addToName($getPost['toName'])
-    //->addTo('bar@foo.com') //One of the most notable changes is how `addTo()` behaves. We are now using our Web API parameters instead of the X-SMTPAPI header. What this means is that if you call `addTo()` multiple times for an email, **ONE** email will be sent with each email address visible to everyone.
-    ->setFrom($getPost['sendFrom'])
-    ->setFromName($getPost['fromName'])
-    ->setSubject($getPost['subject'])
-    ->setText($getPost['msg'])
-    ->setHtml($getPost['msgHTML']);
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
 
-try {
-    $sendgrid->send($email);
-    echo json_encode(array('success' => true, 'message' => "done"));
-} catch (\SendGrid\Exception $e) {
-    $err = $e->getCode() . "\n";
-    foreach ($e->getErrors() as $er) {
-        $err = $err . $er . "\n";
-    }
-    echo json_encode(array('success' => false, 'message' => $err));
-}
+$response = $sg->client->mail()->send()->post($mail);
+echo json_encode(array('success' => true, 'message' => $response->statusCode() . " : " . $response->headers() . "\n" . $response->body()));
